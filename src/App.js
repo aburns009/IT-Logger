92502b47651beb095ac16a2bacf3b6feda2a6e8b
aburns009 <aburns009@gmail.com>
2021-02-17 14:18:04 -0800

    'Add tech' and 'log modal' functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
@@ -15,6 +17,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
